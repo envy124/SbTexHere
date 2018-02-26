83450a557aaeb017356b11c8e7f9125d2214be8c
Alexander Britov <pkgsail.dev@gmail.com>
2018-02-26 11:26:11 +0300

    HttpClientBuilderFactory added

--- app/src/main/java/int09h/facebook/com/sbtexhere3/api/Sberbank.kt
+++ app/src/main/java/int09h/facebook/com/sbtexhere3/api/Sberbank.kt
@@ -1,6 +1,9 @@
 package int09h.facebook.com.sbtexhere3.api
 
 import android.util.Log
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import int09h.facebook.com.sbtexhere3.models.SbAtmResponse
 import int09h.facebook.com.sbtexhere3.models.Triangle
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
@@ -15,37 +18,46 @@ import java.io.IOException
 const val SB_ENDPOINT = "https://sberbank.ru/portalserver/proxy/"
 const val SB_GET_ATMS = "http://localhost/oib-rs/byBounds/entities"
 
-class Sberbank(private val host: String = SB_ENDPOINT) {
+class Sberbank(
+        private val sbApi: String = SB_ENDPOINT,
+        private val httpClient: OkHttpClient = OkHttpClient()
+) {
     private val TAG = Sberbank::class.java.simpleName
+    private val moshi = Moshi.Builder().build()
+    private val atmList = Types.newParameterizedType(List::class.java, SbAtmResponse::class.java)!!
+    private val getAtmsJsonAdapter = moshi.adapter<ArrayList<SbAtmResponse>>(atmList)
 
-    private val client: OkHttpClient = OkHttpClient()
-
-    fun getAtms(geo: Triangle, size: Int = 0, page: Int = 0) {
-        val internalMethod = okhttp3.HttpUrl.parse(SB_GET_ATMS)?.newBuilder()
-                ?.addQueryParameter("size", size.toString())
-                ?.addQueryParameter("page", page.toString())
-                ?.addQueryParameter("filter[type][]", "itt")
-                ?.addQueryParameter("filter[type][]", "atm")
-                ?.addQueryParameter("filter[flags][forPrivate]", "1")
+    fun getAtms(geo: Triangle, size: Int = 9, page: Int = 0) {
+        val internalMethod = okhttp3.HttpUrl.parse(SB_GET_ATMS)?.newBuilder()!!
+                .addQueryParameter("size", size.toString())
+                .addQueryParameter("page", page.toString())
+                .addQueryParameter("filter[type][]", "itt")
+                .addQueryParameter("filter[type][]", "atm")
+                .addQueryParameter("filter[flags][forPrivate]", "1")
 
         for ((name, value) in geo.serialize()) {
-            internalMethod?.addQueryParameter(name, value)
+            internalMethod.addQueryParameter(name, value)
         }
+//
+//        val internalMethodUrl = internalMethod.toString() + "&filter[type][]=itt" +
+//                "&filter[type][]=atm&filter[flags][forPrivate]=1"
 
-        val method = HttpUrl.parse(SB_ENDPOINT)?.newBuilder()
-                ?.addQueryParameter("pipe", "branchesPipe")
-                ?.addQueryParameter("url", internalMethod.toString())
-                ?.build()
+        val method = HttpUrl.parse(sbApi)?.newBuilder()!!
+                .addQueryParameter("pipe", "branchesPipe")
+                .addQueryParameter("url", internalMethod.build().url().toString())
+                .build()
 
         val request = Request.Builder()
                 .url(method)
                 .build()
 
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code " + response)
-            val body = response.body()
-
-            Log.d(TAG, body.toString())
-        }
+//        httpClient.newCall(request).execute().use { response ->
+//            if (!response.isSuccessful) throw IOException("Unexpected code " + response)
+//            val bodyStr = response.body()?.string()
+//            val data = getAtmsJsonAdapter.fromJson(bodyStr)
+////            val data = getAtmsJsonAdapter.fromJson(response.body()?.source())
+//
+//            Log.d(TAG, data!![0].address)
+//        }
     }
 }
--- app/src/test/java/int09h/facebook/com/sbtexhere3/SbUnitTest.kt
+++ app/src/test/java/int09h/facebook/com/sbtexhere3/SbUnitTest.kt
@@ -4,8 +4,8 @@ import int09h.facebook.com.sbtexhere3.api.Sberbank
 import int09h.facebook.com.sbtexhere3.models.Point
 import int09h.facebook.com.sbtexhere3.models.Triangle
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,14 +13,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class SbUnitTest {
+
     @Test
     fun getAtmQuery_isCorrect() {
+        val proxy = Proxy(Proxy.Type.SOCKS, InetSocketAddress("192.168.0.104", 8889))
+        val client = HttpClientBuilderFactory().createHttpClientBuilder()
+                .proxy(proxy)
+                .build()
         val api = Sberbank()
 //        val geo = Triangle.createFromPointAndRadius(Point(0.0f, 0.0f), 3.14159f)
         val geo = Triangle(
-                Point(55.78630908896398f, 37.67243220355226f),
-                Point(55.79839815416208f, 37.71268679644777f),
-                Point(55.750518899999996f, 37.9878976f))
+                Point(55.78630908896398, 37.67243220355226),
+                Point(55.79839815416208, 37.71268679644777),
+                Point(55.750518899999996, 37.9878976))
         api.getAtms(geo)
     }
 }
