20d80a4911b8d3e2c621b2d921b7bff85b436e1a
Alexander Britov <pkgsail.dev@gmail.com>
2018-02-27 11:08:41 +0300

    triangle from point fixed

--- app/src/main/java/int09h/facebook/com/sbtexhere3/models/Geo.kt
+++ app/src/main/java/int09h/facebook/com/sbtexhere3/models/Geo.kt
@@ -6,7 +6,20 @@ import org.json.JSONObject
  * Created by int09h on 23/02/2018.
  */
 
-data class Point (val latitude: Double, val longitude: Double)
+data class Point (var latitude: Double, var longitude: Double)
+
+data class Vector(val point1: Point, val point2: Point) {
+
+    fun rotate(angle: Double): Point {
+        val centerX = point1.longitude
+        val centerY = point1.latitude
+        val point2x = point2.longitude
+        val point2y = point2.latitude
+        val newX = centerX + (point2x-centerX)*Math.cos(angle) - (point2y-centerY)*Math.sin(angle)
+        val newY = centerY + (point2x-centerX)*Math.sin(angle) + (point2y-centerY)*Math.cos(angle)
+        return Point(newY, newX)
+    }
+}
 
 data class Triangle (val left: Point, val right: Point, val cb: Point) {
 
@@ -29,11 +42,16 @@ data class Triangle (val left: Point, val right: Point, val cb: Point) {
     }
 
     companion object {
-        fun createFromPointAndRadius(point: Point, radius: Float): Triangle {
-            val left = Point(0.0, 0.0)
-            val right = Point(0.0, 0.0)
-            val cornerBack = Point(0.0, 0.0)
-            return Triangle(left, right, cornerBack)
+        fun createFromPointAndRadius(center: Point, radius: Double): Triangle {
+            // This is the goofiest way to get triangle from point, but I don't care
+            val cb = Point(center.latitude + radius, center.longitude)
+            val radiusVector = Vector(center, cb)
+            val left = radiusVector.rotate(-120.0)
+            val right = radiusVector.rotate(120.0)
+            left.latitude -= radius / 2
+            right.latitude -= radius / 2
+            cb.latitude -= radius * 1.5
+            return Triangle(left, right, cb)
         }
     }
 }
