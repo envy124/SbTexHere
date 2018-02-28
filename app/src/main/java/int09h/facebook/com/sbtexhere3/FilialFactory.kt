43eaf9d60f798f003fc77ffc93d22c13f2587b48
Alexander Britov <pkgsail.dev@gmail.com>
2018-02-28 10:16:56 +0300

    atms -> filials

--- app/src/main/java/int09h/facebook/com/sbtexhere3/FilialFactory.kt
+++ app/src/main/java/int09h/facebook/com/sbtexhere3/FilialFactory.kt
@@ -11,8 +11,8 @@ import int09h.facebook.com.sbtexhere3.models.Triangle
 class FilialFactory(private val api: Sberbank) {
     fun getFilials(currentPos: Point, radius: Double): List<SbEntity>? {
         val geo = Triangle.createFromPointAndRadius(currentPos, radius)
-        val atms = api.fetchFilials(geo)?.distinctBy { it.address }
-        return atms?.map {
+        val filials = api.fetchFilials(geo)?.distinctBy { it.address }
+        return filials?.map {
             val atmPos = Point(it.coordinates?.latitude!!, it.coordinates.longitude!!)
             SbEntity(it.address!!, it.type!!, it.name!!, currentPos.distanceTo(atmPos) )
         }?.sortedBy { it.distance }
